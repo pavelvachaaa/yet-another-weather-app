d62658e0c77dd74535f94ba569b7cce46aaa7328
Pavel Vácha <pavel.vachaaa@gmail.com>
2025-04-06 16:53:06 +0200

    feat: .

--- src/main/java/org/pavelvachacz/yetanotherweatherapp/configurations/AppConfiguration.java
+++
@@ -1,9 +0,0 @@
-package org.pavelvachacz.yetanotherweatherapp.configurations;
-
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-public class AppConfiguration {
-
-   
-}
--- src/main/java/org/pavelvachacz/yetanotherweatherapp/controllers/CountryController.java
+++
@@ -1,7 +0,0 @@
-package org.pavelvachacz.yetanotherweatherapp.controllers;
-
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-public class CountryController {
-}
