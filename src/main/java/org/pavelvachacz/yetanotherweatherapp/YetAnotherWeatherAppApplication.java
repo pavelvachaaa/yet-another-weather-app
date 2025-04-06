24749001acdef31e21254f80168ca3208097201f
Pavel Vácha <pavel.vachaaa@gmail.com>
2025-04-06 13:08:58 +0200

    feat: moje chyba. znova init

---
+++ src/main/java/org/pavelvachacz/yetanotherweatherapp/YetAnotherWeatherAppApplication.java
@@ -0,0 +1,13 @@
+package org.pavelvachacz.yetanotherweatherapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YetAnotherWeatherAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YetAnotherWeatherAppApplication.class, args);
+	}
+
+}
